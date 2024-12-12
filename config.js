873c38bfa22d446f9443f1680c5a10191d1733dc
Zenreck <kh4rljhayhanbernal@gmail.com>
2024-12-11 18:07:26 -0800

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,18 @@ const config = {
     // You can add multiple users by doing username: 'password'.
     interstellar: "1212",
     JJ: "Pro",
+    Kharl: "Conexion",
+    Jared: "Meisjared",
+    Geo: "God",
+    Legend: "123",
+    Eddie: "Crafter",
+    jamaal_2343: "12534089",
+    hamburger: "food123",
+    tahjari: "tahjarihughes2012",
+    Mark: "skibidi",
+    Ivan: "kaizen",
+    Joshua: "12345",
+    Geust: "abc123",
   },
 };
 
